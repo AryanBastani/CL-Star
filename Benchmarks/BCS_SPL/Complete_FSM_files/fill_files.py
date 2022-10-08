f0d2671ded2bc810a1a3bda3f4e4ec4653b3762b
f.labbaf98 <f.labbaf98@gmail.com>
2022-10-08 15:15:37 +0330

    add results

--- Benchmarks/BCS_SPL/Complete_FSM_files/fill_files.py
+++ Benchmarks/BCS_SPL/Complete_FSM_files/fill_files.py
@@ -6,14 +6,14 @@ with open('./products/file_names.txt') as f:
         if line != "":
             list_of_names.append(line)
 
-for i in range(2, 10):
+for i in range(6, 8):
     data = list(itertools.combinations(list_of_names, i))
-    num = min(len(data), 20)
+    num = min(len(data), 40)
     samples = random.sample(data, num)
     c = 0
     for s in samples:
         c +=1
-        name = "./products/" + str(i) + "wise_" + str(c) + ".txt"
+        name = "./products2/" + str(i) + "wise_" + str(c) + ".txt"
         with open(name, 'w+') as fp:
             for item in s:
                 # write each item on a new line
